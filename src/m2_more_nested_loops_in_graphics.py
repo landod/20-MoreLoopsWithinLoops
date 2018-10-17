f14b8891cdb0528130e9649e3acb48e7faa66803
landod <landod@rose-hulman.edu>
2018-10-17 18:10:49 -0400

    asdf

--- src/m2_more_nested_loops_in_graphics.py
+++ src/m2_more_nested_loops_in_graphics.py
@@ -3,8 +3,8 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of TWO-DIMENSIONAL GRAPHICS.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Owen Land.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -49,9 +49,37 @@ def draw_upside_down_wall(rectangle, n, window):
     and n is nonnegative.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     Some tests are already written for you (above).
     # ------------------------------------------------------------------
+
+    rectangle.attach_to(window)
+    window.render()
+    new_rect = rectangle.clone()
+    x = new_rect.get_upper_right_corner()
+    y = new_rect.get_lower_left_corner()
+    h = new_rect.get_height()
+    w = new_rect.get_width()
+
+    for j in range(1, n):
+        for k in range(j+1):
+            rect = rg.Rectangle(rg.Point(x.x - (w/2 * j) * ((-1) ** k), x.y - (h * (j-1)) - h), rg.Point(y.x - (w/2 * j)
+                                        * ((-1) ** k), y.y - (h * (j-1)) - h))
+            rect.attach_to(window)
+            window.render()
+            if j % 2 == 1:
+                rect1 = rg.Rectangle(rg.Point(x.x, x.y - h * (j + 1) + 2 * h), rg.Point(y.x, y.y - h * (j+ 1) + 2 *  h))
+                rect1.attach_to(window)
+                window.render()
+                a = rect1.get_upper_right_corner()
+                b = rect1.get_lower_left_corner()
+
+                for z in range(n - j + 1):
+                    for i in range(z + 1):
+                        new_rect = rg.Rectangle(rg.Point(a.x - (w/2 * z) * ((-1) ** i), a.y - (h * (z-1)) - h),
+                                                rg.Point(b.x - (w/2 * z) * ((-1) ** i), b.y - (h * (z-1)) - h))
+                        new_rect.attach_to(window)
+                        window.render()
 
 
 # ----------------------------------------------------------------------
